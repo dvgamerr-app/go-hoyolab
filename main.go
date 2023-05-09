80167debf11ed2a69e887966e4eede338528891a
Kananek Thongkam <info.dvgamer@gmail.com>
2023-05-09 17:54:47 +0700

    refector: allow specific profile

--- main.go
+++ main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"path"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	"github.com/go-resty/resty/v2"
 	"github.com/zellyn/kooky"
 	"github.com/zellyn/kooky/browser/chrome"
-	"gopkg.in/yaml.v2"
 )
 
 var configExt string = "yaml"
@@ -42,6 +40,7 @@ func init() {
 		configPath = path.Join(dirname, configPath)
 		logPath = path.Join(dirname, logPath)
 	}
+	log.SetFlags(log.Lshortfile | log.Ltime)
 	if !IsDebug {
 		log.SetFlags(log.Ldate | log.Ltime)
 		f, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -57,112 +56,155 @@ func main() {
 		defer logfile.Close()
 	}
 
-	installSchedule := *flag.Bool("install", false, "# install to ")
-	flag.Parse()
+	// installSchedule := *flag.Bool("install", false, "# install to ")
+	// flag.Parse()
 
-	if installSchedule {
-		ex, err := os.Executable()
-		if err != nil {
-			panic(err)
-		}
-		exPath := filepath.Dir(ex)
-		fmt.Println(exPath)
+	// if installSchedule {
+	// 	ex, err := os.Executable()
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	exPath := filepath.Dir(ex)
+	// 	fmt.Println(exPath)
+	// }
+	hoyo := GenerateDefaultConfig()
+	apiHoyo, err := hoyo.ReadHoyoConfig()
+	if err != nil {
+		log.Fatal(err)
 	}
+	hoyo.Daily = apiHoyo
 
-	hoyo := &Hoyolab{
-		Client:    resty.New(),
+	cookieStore := kooky.FindAllCookieStores()
+	log.Printf("Browser total %d sessions", len(cookieStore))
+	for _, store := range cookieStore {
+		_, err = os.Stat(store.FilePath())
+		if os.IsNotExist(err) {
+			continue
+		}
+
+		for _, act := range hoyo.Browser {
+			if len(act.Profile) > 0 && (store.Browser() != act.Browser || !ContainsStrings(act.Profile, store.Profile())) {
+				continue
+			}
+			log.Printf("Allow '%s' :: '%s'", store.Browser(), store.Profile())
+
+			_, err := chrome.CookieJar(store.FilePath())
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		}
+
+		// 	uri, _ := url.Parse(hoyo.Daily.API.Domain)
+		// 	hoyo.SetCookie(cookies.Cookies(uri))
+
+		// 	if len(hoyo.CookieJar) == 0 {
+		// 		log.Printf("%s::Cookie is empty, please login hoyolab.com.", store.Browser())
+		// 		continue
+		// 	}
+
+		// 	resInfo, err := hoyo.DailyInfo()
+		// 	if err != nil {
+		// 		log.Fatalf("Hoyolab::DailyInfo: %v", err)
+		// 	}
+		// 	if resInfo.RetCode != 0 {
+		// 		log.Printf("Hoyolab::DailyInfo: %v", resInfo.Message)
+		// 		continue
+		// 	}
+		// 	// log.Printf("Hoyolab::DailyInfo:%+v", resInfo.Data)
+
+		// 	actInfo, ok := resInfo.Data.(ActInfo)
+		// 	if !ok {
+		// 		log.Fatalf("DailyInfo: %v", err)
+		// 	}
+		// 	if actInfo.IsSign {
+		// 		log.Printf("Hoyolab::DailyInfo:Claimed Today %s (Total %d Claims)", actInfo.Today, actInfo.TotalSignDay)
+		// 		continue
+		// 	}
+
+		// 	_, err = hoyo.DailySign()
+		// 	if err != nil {
+		// 		log.Fatalf("DailySign: %v", err)
+		// 	}
+		// 	// log.Printf("Hoyolab::DailySign:%+v", resInfo.Data)
+	}
+}
+
+func ContainsStrings(a []string, x string) bool {
+	sort.Strings(a)
+	for _, v := range a {
+		if v == x {
+			return true
+		}
+	}
+	return false
+}
+
+func GenerateDefaultConfig() *Hoyolab {
+	// Genshin Impact
+	//
+	// {"code":"ok"}
+	// {"retcode":0,"message":"OK","data":{"total_sign_day":11,"today":"2022-10-29","is_sign":false,"first_bind":false,"is_sub":true,"region":"os_asia","month_last_day":false}}
+	var apiGenshinImpact = &DailyHoyolab{
 		CookieJar: []*http.Cookie{},
-		Daily: &DailyHoyolab{
-			ActID: "e202102251931481",
-			API: DailyAPI{
-				Endpoint: "https://sg-hk4e-api.hoyolab.com",
-				Domain:   "https://hoyolab.com",
-				Sign:     "/event/sol/sign",
-				Info:     "/event/sol/info",
-			},
-			Browser: []BrowserProfile{
-				{
-					Browser:   "chrome",
-					Profile:   []string{"dvgamerr", "dvgamer"},
-					UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
-				},
-			},
-			Lang:    "en-us",
-			Referer: "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html",
+		ActID:     "e202102251931481",
+		API: DailyAPI{
+			Endpoint: "https://sg-hk4e-api.hoyolab.com",
+			Domain:   "https://hoyolab.com",
+			Award:    "/event/sol/home",
+			Info:     "/event/sol/info",
+			Sign:     "/event/sol/sign",
 		},
+		Lang:    "en-us",
+		Referer: "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html",
 	}
 
-	if _, err := os.Stat(configPath); err != nil {
-		raw, err := yaml.Marshal(map[string]interface{}{
-			"config": hoyo.Daily,
-		})
-		if err != nil {
-			log.Panic("yaml Marshal fail")
-		}
-		err = os.WriteFile(configPath, raw, 0644)
-		if err != nil {
-			log.Panic("WriteFile fail")
-		}
-	} else {
-		raw, err := os.ReadFile(configPath)
-		if err != nil {
-			log.Panic("ReadFile fail")
-		}
-
-		var configFile map[string]*DailyHoyolab
-
-		err = yaml.Unmarshal(raw, &configFile)
-		if err != nil {
-			log.Panic("yaml Marshal fail")
-		}
-
-		hoyo.Daily = configFile["config"]
+	// Honkai StarRail
+	//
+	// https://sg-public-api.hoyolab.com/event/luna/os/home?lang=en-us&act_id=e202303301540311
+	// https://sg-public-api.hoyolab.com/event/luna/os/sign
+	// {"retcode":0,"message":"OK","data":{"code":"","risk_code":0,"gt":"","challenge":"","success":0,"is_risk":false}}
+	// https://sg-public-api.hoyolab.com/event/luna/os/info
+	// {"retcode":0,"message":"OK","data":{"total_sign_day":7,"today":"2023-05-09","is_sign":false,"is_sub":false,"region":"","sign_cnt_missed":1,"short_sign_day":0}}
+	//
+	var apiHonkaiStarRail = &DailyHoyolab{
+		CookieJar: []*http.Cookie{},
+		ActID:     "e202303301540311",
+		API: DailyAPI{
+			Endpoint: "https://sg-public-api.hoyolab.com/",
+			Domain:   "https://hoyolab.com",
+			Award:    "/event/luna/os/home",
+			Info:     "/event/luna/os/info",
+			Sign:     "/event/luna/os/sign",
+		},
+		Lang:    "en-us",
+		Referer: "https://act.hoyolab.com/bbs/event/signin/hkrpg/index.html",
 	}
 
-	for _, store := range kooky.FindAllCookieStores() {
-		_, err := os.Stat(store.FilePath())
-
-		if store.Browser() != "chrome" || os.IsNotExist(err) {
-			continue
-		}
-
-		log.Printf("%s::%s", store.Browser(), store.Profile())
-		cookies, err := chrome.CookieJar(store.FilePath())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		uri, _ := url.Parse(hoyo.Daily.API.Domain)
-		hoyo.SetCookie(cookies.Cookies(uri))
-
-		if len(hoyo.CookieJar) == 0 {
-			log.Printf("%s::Cookie is empty, please login hoyolab.com.", store.Browser())
-			continue
-		}
-
-		resInfo, err := hoyo.DailyInfo()
-		if err != nil {
-			log.Fatalf("Hoyolab::DailyInfo: %v", err)
-		}
-		if resInfo.RetCode != 0 {
-			log.Printf("Hoyolab::DailyInfo: %v", resInfo.Message)
-			continue
-		}
-		// log.Printf("Hoyolab::DailyInfo:%+v", resInfo.Data)
-
-		actInfo, ok := resInfo.Data.(ActInfo)
-		if !ok {
-			log.Fatalf("DailyInfo: %v", err)
-		}
-		if actInfo.IsSign {
-			log.Printf("Hoyolab::DailyInfo:Claimed Today %s (Total %d Claims)", actInfo.Today, actInfo.TotalSignDay)
-			continue
-		}
-
-		_, err = hoyo.DailySign()
-		if err != nil {
-			log.Fatalf("DailySign: %v", err)
-		}
-		// log.Printf("Hoyolab::DailySign:%+v", resInfo.Data)
+	// Honkai Impact 3
+	// https: //act.hoyolab.com/bbs/event/signin-bh3/index.html?act_id=e202110291205111
+	var apiHonkaiImpact = &DailyHoyolab{
+		CookieJar: []*http.Cookie{},
+		ActID:     "e202110291205111",
+		API: DailyAPI{
+			Endpoint: "https://sg-public-api.hoyolab.com",
+			Domain:   "https://hoyolab.com",
+			Award:    "/event/mani/home",
+			Sign:     "/event/mani/sign",
+			Info:     "/event/mani/info",
+		},
+		Lang:    "en-us",
+		Referer: "https://act.hoyolab.com/bbs/event/signin-bh3/index.html",
+	}
+	return &Hoyolab{
+		Client: resty.New(),
+		Browser: []BrowserProfile{
+			{
+				Browser:   "chrome",
+				Profile:   []string{"dvgamer", "dvgamerr"},
+				UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+			},
+		},
+		Daily: []*DailyHoyolab{apiGenshinImpact, apiHonkaiStarRail, apiHonkaiImpact},
 	}
 }
